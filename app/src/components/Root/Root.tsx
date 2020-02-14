03406257a4eac7c7a8dec26b3a94d70fff9dd984
Dat Vu <redb1997@gmail.com>
2020-02-14 15:33:38 +0300

    added fe

---
+++ app/src/components/Root/Root.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import styled from "styled-components";
+
+import Chefs from './Chefs';
+
+const Heading = styled.strong`
+  display: block;
+  font-size: 2rem;
+  margin: 0.75rem 0 1em;
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 60rem;
+`;
+
+const Root = () => {
+  return <Wrapper><Heading>Root</Heading>
+    <Chefs /></Wrapper>
+}
+
+export default Root;
